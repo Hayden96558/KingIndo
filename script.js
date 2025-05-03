a9016b52eda73a3ba9fa9ae5956603dbd4965d95
Hayden96558 <thohaydenmelvyn@gmail.com>
2025-05-03 13:49:20 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// // coba buat anga dari 1 sampai 10
+// // var angka1 = "1";
+// // var angka2 = "2";
+// // var angka3 = "3";
+// // var angka4 = "4";
+// // var angka5 = "5";
+// // var angka6 = "6";
+// // var angka7 = "7";
+// // var angka8 = "8";
+// // var angka9 = "9";
+// // var angka10 = "10";
+// // console.log (angka1);
+// // console.log (angka2)
+// // console.log (angka3)
+// // console.log (angka4)
+// // console.log (angka5)
+// // console.log (angka6)
+// // console.log (angka7)
+// // console.log (angka8)
+// // console.log (angka9)
+// // console.log (angka10)
+
+// // mau dari angka berapa
+// // syaratnya apa (rules jika benar maka loopingnya berlanjut, jika salah maka loopingnya berhenti)
+// // langkahnya seperti apa
+
+// // 1
+// // 1 < 10(benar)
+// // 1 
+
+// // baris 1 - 5 angka genap (1, 10)
+// // baris 6 - 10 angka ganjil (1, 10)
+
+// for (var tamansafari = 1; tamansafari < 10; tamansafari=tamansafari+2){
+//     console.log(tamansafari);
+// }
+
+// for (var satwa = 1; satwa > 10; satwa=satwa+2){
+//     console.log(satwa);
+// }
